d3c7cc0003a86974e79e866021ad71c392092aad
Leticiasg <leticiagoncalves2005@gmail.com>
2015-06-17 13:51:04 -0300

    recebendo ibeacons

---
+++ Pushcart/Pushcart/VendingViewController.swift
@@ -0,0 +1,25 @@
+//
+//  VendingViewController.swift
+//  Pushcart
+//
+//  Created by Letícia Gonçalves on 6/16/15.
+//  Copyright (c) 2015 LazyFox. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
